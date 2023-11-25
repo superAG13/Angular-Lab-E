63582fb4e9c0df9249a0fd00b585842fa394dac3
AG <antoni.golabek@gmail.com>
2023-11-25 13:02:54 +0100

    lab a changes

---
+++ lab-a/src/app/details/details.component.ts
@@ -0,0 +1,26 @@
+import {Component, inject} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HousingLocation} from "../housinglocation";
+import {ActivatedRoute} from "@angular/router";
+import {HousingService} from "../housing.service";
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
+
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+      this.housingLocation = housingLocation;
+    });
+  }
+}
+
